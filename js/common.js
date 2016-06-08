53683a251f41ef75a414430754fc468390cb66e9
OlegPrus <oleg.prus.first@gmail.com>
2016-06-08 03:43:40 +0300

    Second Commit

--- js/common.js
+++ js/common.js
@@ -8,5 +8,33 @@
                 $('html, body').animate({scrollTop: $(target).offset().top}, 1000);//800 - длительность скроллинга в мс
                 return false;
             });
+            $('a[href^="#3"]').click(function()
+            {
+                var target = $(this).attr('href'); // взяли значення
+                // атрибута - href в ссилки
+                $('html, body').animate({scrollTop: $(target).offset().top}, 1000);//800 - длительность скроллинга в мс
+                return false;
+            });
+            $('a[href^="#top"]').click(function()
+            {
+                var target = $(this).attr('href'); // взяли значення
+                // атрибута - href в ссилки
+                $('html, body').animate({scrollTop: $(target).offset().top}, 1000);//800 - длительность скроллинга в мс
+                return false;
+            });
         });
     /*Плавный переход*/
+$(window).scroll(function(){
+    var l = $(this).scrollTop();
+    var height_screen = $(window).height();
+    if(l<height_screen){
+        $(".up-btn").css({
+            "opacity" : "0"
+        });
+    }
+    else{
+        $(".up-btn").css({
+            "opacity" : "1"
+        });
+    }
+});
